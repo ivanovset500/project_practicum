af3dd7bf2a3183b00b2185db4030515b85d746da
ivanovset500 <ivanov.set500@yandex.ru>
2026-05-28 18:39:19 +0500

    Добавлена Docker-контейнеризация проекта

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./tickets.db"
+DATABASE_URL = "sqlite:///./data/tickets.db"
 
 engine = create_engine(
     DATABASE_URL,
